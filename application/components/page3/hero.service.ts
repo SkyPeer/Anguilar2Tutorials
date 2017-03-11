886f0c49f546ea70debabbac8986f7992d64a090
Oleg <o.selivancev@gmail.com>
2017-03-12 02:59:04 +0300

    1203

---
+++ application/components/page3/hero.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import {Hero} from "../../entity/hero";
+
+@Injectable()
+export class HeroService {
+    getHeroes(): void {} // stub
+}
---
+++ application/components/page3/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from "../../entity/hero";
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice', telnumb: '+2 31232323-20', },
+    { id: 12, name: 'Narco', telnumb: '+3 231232323-12', },
+    { id: 13, name: 'Bombasto', telnumb: '+1 231232323-13', },
+    { id: 14, name: 'Celeritas', telnumb: '+4 231232323-14', },
+    { id: 15, name: 'Magneta', telnumb: '+7 231232323-15', },
+    { id: 16, name: 'RubberMan', telnumb: '+1 31232323-20', },
+    { id: 17, name: 'Dynama', telnumb: '+0 123231-17', },
+    { id: 18, name: 'Dr IQ', telnumb: '+0 1234567-18', },
+    { id: 19, name: 'Magma', telnumb: '+1 234347-19', },
+    { id: 20, name: 'Tornado', telnumb: '+2 31232323-20', } ]
+
