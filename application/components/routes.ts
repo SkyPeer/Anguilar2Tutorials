b7aa4e16afdd81e77df118ca376a3b522b0625fe
Oleg <o.selivancev@gmail.com>
2017-04-23 21:21:42 +0300

    Worked Project, without Page6
    23042017

--- application/components/routes.ts
+++ application/components/routes.ts
@@ -1,5 +1,6 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
+import {Page5} from "./page5/page5";
 import {Page4} from "./page4/page4";
 import {Page3} from "./page3/page3";
 import {Page2} from "./page2/page2";
@@ -7,7 +8,8 @@ import {Page1} from "./page1/page1";
 import { DashboardComponent }   from './page5/dashboard.component';
 import { HeroesComponent2 }      from './page5/heroes.component';
 import { HeroDetailComponent2 }  from './page5/hero-detail.component';
-import {Page5} from "./page5/page5";
+
+
 
 const routes: Routes = [
     {path: '', redirectTo: '/page1', pathMatch:'full'},
@@ -16,6 +18,7 @@ const routes: Routes = [
     {path: 'page3', component: Page3},
     {path: 'page4', component: Page4},
     {path: 'page5', component: Page5},
+
     { path: 'detail/:id', component: HeroDetailComponent2 },
     { path: 'heroes',     component: HeroesComponent2 },
     { path: 'dashboard',  component: DashboardComponent },
--- application/modules/init.ts
+++ application/modules/init.ts
@@ -1,24 +1,42 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import {Layout} from  '../components/layout/layout'
 import {RoutingModule} from "../components/routes";
-import {Page3} from "../components/page3/page3";
-import {Page2} from "../components/page2/page2";
 import {Page1} from "../components/page1/page1";
+import {Page2} from "../components/page2/page2";
+import {Page3} from "../components/page3/page3";
 import {Page4} from "../components/page4/page4";
 import {Page5} from "../components/page5/page5";
+
 //import {FooterComponent} from "./.";
+
 import {HeroDetailComponent} from "../components/page4/detail/detail";
 import {HeroNumberComponent} from "../components/layout/footer/footer";
 import {HeroService} from "../entity/hero.service";
+
+// page5
 import {DashboardComponent} from "../components/page5/dashboard.component";
 import {HeroDetailComponent2} from "../components/page5/hero-detail.component";
 import {HeroesComponent2} from "../components/page5/heroes.component"
+
+// page6
+
+
+
 @NgModule({
-    declarations: [Layout, Page1, Page2, Page3, Page4, Page5, HeroDetailComponent, HeroNumberComponent, DashboardComponent, HeroDetailComponent2, HeroesComponent2],
-    imports: [BrowserModule, RoutingModule, FormsModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RoutingModule
+    ],
+    declarations:
+        [
+        Layout, Page1, Page2, Page3, Page4, Page5, HeroDetailComponent, HeroNumberComponent, DashboardComponent, HeroDetailComponent2, HeroesComponent2,
+        ],
     providers:[HeroService, Page5],
     bootstrap: [Layout]
 })
